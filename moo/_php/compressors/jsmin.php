b8437432105d99653b0934f12d2feef0799a8103
alari <name.alari@gmail.com>
2010-02-22 21:30:41 +0000

    fixed jsmin compressor path
    git-svn-id: svn://svn.orena.org/orena/trunk@520 d1990dbc-1c88-9740-88cc-0edcd7010ae0

--- moo/_php/compressors/jsmin.php
+++ moo/_php/compressors/jsmin.php
@@ -1,7 +1,7 @@
 <?php
 
 function jsmin($script, $path, $root){
-	include_once $root.'compressors/jsmin-1.1.1.php';
+	include_once $root.'_php/compressors/jsmin-1.1.1.php';
 	return JSMin::minify($script);
 }
 
