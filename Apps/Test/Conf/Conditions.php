970389b5296d945c9eae723448f9522067c10977
alari <name.alari@gmail.com>
2010-03-22 14:35:23 +0000

    git-svn-id: svn://svn.orena.org/orena/trunk@614 d1990dbc-1c88-9740-88cc-0edcd7010ae0

--- Apps/Test/Conf/Conditions.php
+++ Apps/Test/Conf/Conditions.php
@@ -2,7 +2,7 @@
 O("*conditions", function(){
 	if(O("~http_host") == "test.orena.org") {
 		O("*mode", "testing");
-		O("_app/db/default", Array(
+		O("_db/default", Array(
 			"engine"=>"mysql",
 			"host"=>"localhost:3306",
 			"dbname"=>"orena_tests",
@@ -12,7 +12,7 @@ O("*conditions", function(){
 	} elseif(O("~http_host") == "orena.dev") {
 		O("*mode", "testing");
 		// no db yet
-		O("_app/db/default", Array("engine"=>"-"));
+		O("_db/default", Array("engine"=>"-"));
 	}
 	if(O("*mode")){
 		O("_prefix", "Test");
